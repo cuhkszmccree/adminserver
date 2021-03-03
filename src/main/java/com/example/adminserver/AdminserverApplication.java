847ff2d6e79c774185310e7f231bc2d64b8075d9
cuhkszmccree <115010147@link.cuhk.edu.cn>
2021-03-02 18:16:25 -0800

    newest_version

--- src/main/java/com/example/adminserver/AdminserverApplication.java
+++ src/main/java/com/example/adminserver/AdminserverApplication.java
@@ -10,6 +10,7 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 public class AdminserverApplication {
 
     public static void main(String[] args) {
+        System.out.println("adminserver_start");
         SpringApplication.run(AdminserverApplication.class, args);
     }
 
